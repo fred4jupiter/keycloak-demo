7eb0dbc2424324b5ab0efa6fddc12f2f81a3870a
Staehler, Michael <michael.staehler@opitz-consulting.com>
2018-01-15 19:29:55 +0100

    update app for using 2 instances for demo

--- src/main/java/com/fred4jupiter/keycloak/keycloakdemo/web/IndexController.java
+++ src/main/java/com/fred4jupiter/keycloak/keycloakdemo/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.fred4jupiter.keycloak.keycloakdemo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by exmstae on 12.07.2017.
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+	@Value("${spring.application.name}")
+	private String applicationName;
+
 	@GetMapping(path = "/")
-	public String showIndexPage(){
-		return "index";
+	public ModelAndView showIndexPage() {
+		return new ModelAndView("index", "applicationName", applicationName);
 	}
 }
