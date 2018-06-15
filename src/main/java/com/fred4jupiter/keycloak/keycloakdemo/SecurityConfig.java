a5bd42b46761b07cb157d43d56e7899d4985ad89
Staehler, Michael <michael.staehler@opitz-consulting.com>
2018-06-15 15:42:57 +0200

    refactored url filtering

--- src/main/java/com/fred4jupiter/keycloak/keycloakdemo/SecurityConfig.java
+++ src/main/java/com/fred4jupiter/keycloak/keycloakdemo/SecurityConfig.java
@@ -26,46 +26,42 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-	/**
-	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
-	 */
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
 
-		SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
-		simpleAuthorityMapper.setPrefix("");
-		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+        simpleAuthorityMapper.setPrefix("");
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
 
-		auth.authenticationProvider(keycloakAuthenticationProvider);
-	}
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
 
-	@Bean
-	public KeycloakConfigResolver keycloakConfigResolver() {
-		return new KeycloakSpringBootConfigResolver();
-	}
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
-	/**
-	 * Defines the session authentication strategy.
-	 */
-	@Bean
-	@Override
-	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		return new RegisterSessionAuthenticationStrategy(sessionRegistry());
-	}
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(sessionRegistry());
+    }
 
-	@Bean
-	public SessionRegistry sessionRegistry() {
-		return new SessionRegistryImpl();
-	}
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-
-		http.authorizeRequests().antMatchers("/products*").hasAuthority("PERM_PRODUCTS");
-		http.authorizeRequests().antMatchers("/simple*").permitAll();
-
-		http.authorizeRequests().anyRequest().permitAll();
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().antMatchers("/products*").hasAuthority("PERM_PRODUCTS");
+    }
 }
