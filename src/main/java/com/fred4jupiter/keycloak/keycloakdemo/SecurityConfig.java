b8bf2a145bcd73551e63176b7aac9a17a418a2c1
Staehler, Michael <michael.staehler@opitz-consulting.com>
2018-06-22 09:16:15 +0200

    simplified configuration

--- src/main/java/com/fred4jupiter/keycloak/keycloakdemo/SecurityConfig.java
+++ src/main/java/com/fred4jupiter/keycloak/keycloakdemo/SecurityConfig.java
@@ -2,18 +2,12 @@ package com.fred4jupiter.keycloak.keycloakdemo;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -21,9 +15,7 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 /**
  * Created by exmstae on 13.07.2017.
  */
-@Configuration
-@EnableWebSecurity
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+@KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     /**
@@ -31,13 +23,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-
-        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
-        simpleAuthorityMapper.setPrefix("");
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
-
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+        auth.authenticationProvider(keycloakAuthenticationProvider());
     }
 
     @Bean
@@ -51,17 +37,14 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(sessionRegistry());
-    }
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.authorizeRequests().antMatchers("/products*").hasAuthority("PERM_PRODUCTS");
+
+        http.authorizeRequests().antMatchers("/products*").hasAuthority("ROLE_PRODUCTS");
+        http.authorizeRequests().antMatchers("/**").permitAll();
     }
 }
